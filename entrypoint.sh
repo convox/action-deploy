4b76e7908354ab2744e0f86d9aa8df37534ea7a8
Ed Fricker <edafricker@gmail.com>
2023-03-17 14:35:09 +0000

    Allow for running with --no-cache flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,15 @@
 echo "Deploying"
 if [ -n "$INPUT_PASSWORD" ]
 then
- export CONVOX_PASSWORD=$INPUT_PASSWORD
+    export CONVOX_PASSWORD=$INPUT_PASSWORD
 fi
 if [ -n "$INPUT_HOST" ]
 then
- export CONVOX_HOST=$INPUT_HOST
+    export CONVOX_HOST=$INPUT_HOST
 fi
 export CONVOX_RACK=$INPUT_RACK
-convox deploy --app $INPUT_APP --description "$INPUT_DESCRIPTION" --wait
+if [ "$INPUT_CACHED" = "false" ]; then
+    convox deploy --app $INPUT_APP --description "$INPUT_DESCRIPTION" --no-cache --wait
+else
+    convox deploy --app $INPUT_APP --description "$INPUT_DESCRIPTION" --wait
+fi
