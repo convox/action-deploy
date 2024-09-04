c863d845501c612ee714c7a552e32d12e89082c3
Geir Arnesen <geir.arnesen@schibsted.no>
2024-09-04 10:32:38 +0200

    chore: adjusting parsing of buildargs

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,10 +24,15 @@ fi
 
 # Split the INPUT_BUILDARGS by newline into an array
 if [ "$INPUT_BUILDARGS" != "" ]; then
-    IFS=$'\n' read -d '' -r -a ADDR <<< "$INPUT_BUILDARGS"
-    for i in "${ADDR[@]}"; do
-        # process each build argument and append it to the command
-        BUILDARGS_COMMAND="$BUILDARGS_COMMAND --build-args $i"
+    IFS=$'\n' read -d '' -r -a ADDR <<< "$INPUT_BUILDARGS"  # Split build arguments by newline
+
+    for ARG in "${ADDR[@]}"; do
+        # Extract key and value (handle empty lines or invalid format)
+        KEY=${ARG%%=*}
+        VALUE=${ARG#*=}
+        if [[ -n "$KEY" && -n "$VALUE" ]]; then
+            BUILDARGS_COMMAND="$BUILDARGS_COMMAND --build-args $KEY=$VALUE"
+        fi
     done
 fi
 
